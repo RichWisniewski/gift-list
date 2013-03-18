fc6ba7f8333985bbf107dbb54afd0d3e72d1fe86
Rich Wisniewski <richwisniewski@gmail.com>
2013-03-18 11:33:51 +0000

    Remove subdirectory gift-list which caused Heroku deploy to fail

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GiftList::Application.config.secret_token = '165392ea79bb012eb7387a9acd9a0126cba4f823d53de037157be2bfbc52a757d855c4301364c0f4096c2a7f93f5e099147c67f4271cfe899f7f5c61589707bf'
