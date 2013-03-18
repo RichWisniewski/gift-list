e03718b108372d0d670f1799bc9a379e9eee2ab9
Rich Wisniewski <richwisniewski@gmail.com>
2013-03-18 11:27:30 +0000

    Revert "Update"
    This reverts commit 29f52cce254e0f8efe862c68e64963f1bf5a3cfb.

--- gift-list/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-GiftList::Application.config.secret_token = '9cfe9171808b2bf70cccc03ccb274c627b29196762edf7f74de40b15c54f09e0615491a8a2c1c9bca41554d537f7641479a54ae5742a40b9756e95baa1b9b645'
